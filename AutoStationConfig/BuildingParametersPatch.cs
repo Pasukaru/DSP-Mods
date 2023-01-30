ca99bb5080ed1047c5da1ae940d3f35961194769
Soar Qin <soarchin@gmail.com>
2023-01-30 15:30:19 +0800

    remove extra copy-check that prevents dispenser from filling bots on copying

--- AutoStationConfig/BuildingParametersPatch.cs
+++ AutoStationConfig/BuildingParametersPatch.cs
@@ -44,9 +44,7 @@ namespace Pasukaru.DSP.AutoStationConfig
             if (dispenserId == 0) return;
             var dispenserComponent = factory.transport.dispenserPool[dispenserId];
             if (dispenserComponent == null) return;
-            /* Don't deal with Dispensers with item filter set, which indicates a copy-build */
             dispenserComponent.ApplyGuessFilter(factory);
-            if (filterId != 0) return;
             var itemProto = LDB.items.Select(entityPool[entityId].protoId);
             /* Check if any parameter is not the default value, which indicates a copy-build */
             if (parameters == null ||
